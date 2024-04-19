1dff528fcf462173549cff757b45beab849b0cad
Sajni Bercel <sz.krisztian.suli128@gmail.com>
2024-04-19 12:38:09 +0200

    rename

--- Cowboy/Classes/Weapon.cs
+++ Cowboy/Classes/Weapon.cs
@@ -9,24 +9,21 @@ namespace Cowboy.Classes
     {
         private Player OwnerPlayer { get; set; }
         public int BulletSpeed { get; set; }
-        private int WeaponDamage { get; set; }
+        private int BulletDamage { get; set; }
         private bool CanShoot = false;
         private Timer MyTimer;
 
         /// <summary>
         /// Fegyver ami egy játékoshoz tartozik, ez kezeli közvetlen a lövés, ő hozza létre a töltényt minden tulajdonságával
         /// </summary>
-        /// <param name="player">tulajdonos (használó)</param>
-        /// <param name="realoadSpeed">idő amennyinek el kell tellnie hogy újra lehessen lőni</param>
-        /// <param name="bulletSpeed">a töltény sebessége</param>
-        /// <param name="weaponDamage">Sebzés</param>
+        /// <param name="player">"Tulajdonos" Player (használó)</param>
         public Weapon(Player player)
         {
             OwnerPlayer = player;
 
             BulletSpeed = player.PlayerSettings.BulletSpeed;
 
-            WeaponDamage = player.PlayerSettings.BulletDamage;
+            BulletDamage = player.PlayerSettings.BulletDamage;
 
             MyTimer = new Timer();
             MyTimer.Interval = player.PlayerSettings.ReloadSpeed*100;
@@ -48,7 +45,7 @@ namespace Cowboy.Classes
 
                 Bullet bullet = new Bullet(OwnerPlayer.PlayerID,
                     Create.pictureBox("Bullet", new Size(30, 30), new Point(0, 0),
-                    Properties.Resources.bullet), BulletSpeed, WeaponDamage);
+                    Properties.Resources.bullet), BulletSpeed, BulletDamage);
                 Point offset = OwnerPlayer.GetWeaponOffSetPoint();
                 offset.Offset(0,-bullet.pictureBox.Height/2);
                 bullet.pictureBox.Location = offset;
